5ff52edc17b9b3e5b93b71968b549ced57a7356c
Jim <1255905516@qq.com>
2020-08-19 17:27:00 +0800

    1

--- src/Requests.php
+++ src/Requests.php
@@ -40,105 +40,72 @@ class Requests
         return self::$_instance;
     }
 
-    public function get(string $url, array $data = [], array $headers = [], array $options = [])
+    /**
+     * get请求
+     * @param string $url 目标网址
+     * @param array $data 需要请求的数据
+     * @param array $headers headers信息
+     * @return bool|string
+     */
+    public function get(string $url, array $data = [], array $headers = [])
     {
-        $response =  $this->sendRequest($url,$data,$headers,'get',$options);
-        return $response;
-    }
+        $curl = curl_init(); //初始化CURL句柄
+        curl_setopt($curl, CURLOPT_URL, $url); //设置请求的URL
 
-    public function post(string $url, array $data = [], array $headers = [], array $options = [])
-    {
-        $response =  $this->sendRequest($url,$data,$headers,'post',$options);
-        return $response;
-    }
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($curl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+        }
+        if (!empty($data)) {
+            curl_setopt($curl, CURLOPT_HTTPGET, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        }
+        // 请求的时候 携带的header参数 比如：token，authorization，cookie 等等....
+        if ($headers != null && is_array($headers) && !empty($headers)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0); //设为TRUE把curl_exec()结果转化为字串，而不是直接输出
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT"); //设置请求方式
 
+        $output = curl_exec($curl);
+//        $status = curl_getinfo($curl);
+        curl_close($curl);
+        return $output;
+    }
 
     /**
-     * CURL发送Request请求,含POST和REQUEST
-     * @param string $url 请求的链接
-     * @param mixed $params 传递的参数
-     * @param string $method 请求的方法
-     * @param mixed $options CURL的参数
-     * @return array
+     * post 请求目标地址
+     * @param string $url 目标网址
+     * @param array $data 需要请求的数据
+     * @param array $headers headers信息
+     * @return bool|string
      */
-    private function sendRequest($url, $params = [], $headers = [], $method = 'POST', $options = [])
+    public function post(string $url, array $data = [], array $headers = [])
     {
-        $method = strtoupper($method);
-        $protocol = substr($url, 0, 5);
-        $query_string = is_array($params) ? http_build_query($params) : '';
+        $curl = curl_init();
 
-        $ch = curl_init();
-        $defaults = [];
-        if ('GET' == $method) {
-            $geturl = $query_string ? $url . (stripos($url, "?") !== false ? "&" : "?") . $query_string : $url;
-            $defaults[CURLOPT_URL] = $geturl;
-
-        } else {
-            $defaults[CURLOPT_URL] = $url;
-            if ($method == 'POST') {
-                $defaults[CURLOPT_POST] = 1;
-            } else {
-                $defaults[CURLOPT_CUSTOMREQUEST] = $method;
-            }
-            $defaults[CURLOPT_POSTFIELDS] = $params;
-        }
-        // 携带headers 请求
-        if (is_array($headers) && !empty($headers)) {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        if (!empty($data)) {
+            curl_setopt($curl, CURLOPT_HTTPGET, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
         }
 
-        $defaults[CURLOPT_HEADER] = false; //不取得返回头信息
-        $defaults[CURLOPT_USERAGENT] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.98 Safari/537.36"; // 协议头
-        $defaults[CURLOPT_FOLLOWLOCATION] = true; //应该是跟随重定向的吧
-        $defaults[CURLOPT_RETURNTRANSFER] = true; //返回的内容作为变量储存，而不是直接输出。
-        $defaults[CURLOPT_CONNECTTIMEOUT] = 3;  // 用来告诉 PHP 在成功连接服务器前等待多久（连接成功之后就会开始缓冲输出），这个参数是为了应对目标服务器的过载，下线，或者崩溃等可能状况。
-        $defaults[CURLOPT_TIMEOUT] = 3;  //CURLOPT_TIMEOUT 默认为0，意思是永远不会断开链接。所以不设置的话，可能因为链接太慢，会把 HTTP 资源用完。
-
-        // disable 100-continue
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-
-        if ('https' == $protocol) {
-            $defaults[CURLOPT_SSL_VERIFYPEER] = false;
-            $defaults[CURLOPT_SSL_VERIFYHOST] = false;
+        if (!empty($header)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($curl, CURLOPT_HEADER, 0);//返回response头部信息
         }
 
-        curl_setopt_array($ch, (array)$options + $defaults);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 
-        $ret = curl_exec($ch);
-        $err = curl_error($ch);
 
-        echo json_decode($ret,true);
-        die(1);
-        if (false === $ret || !empty($err)) {
-            $errno = curl_errno($ch);
-            $info = curl_getinfo($ch);  //状态信息
-            curl_close($ch);
-            return [
-                'ret' => false,
-                'errno' => $errno,
-                'msg' => $err,
-                'info' => $info,
-            ];
-        }
-        curl_close($ch);
-        return [
-            'ret' => true,
-            'msg' => $ret,
-        ];
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
     }
 
-
 }
-
-$requests = Requests::getInstance();
-$url = 'https://apinew.juejin.im/tag_api/v1/query_topic_list';
-$url = 'https://apinew.juejin.im/user_api/v1/user/get';
-
-$data['aid'] = 2608;
-$data['user_id'] = '1987506650747293';
-$data['not_self'] = 1;
-$response = $requests->get($url,$data);
-//$response = $requests->post($url,$data);
-
-var_dump($response);
 
